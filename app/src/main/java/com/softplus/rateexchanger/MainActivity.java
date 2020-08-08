42ab18cf1229eca15bc4d4ce0002b96a8c9b0e6a
SoftPlus <rolling3369@gmail.com>
2020-08-08 18:04:11 +0800

    Add gradle build settings

--- app/src/main/java/com/softplus/rateexchanger/MainActivity.java
+++ app/src/main/java/com/softplus/rateexchanger/MainActivity.java
@@ -2,13 +2,43 @@ package com.softplus.rateexchanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import static com.softplus.rateexchanger.utilities.Constants.APP_KEY;
+import static com.softplus.rateexchanger.utilities.Constants.BASE_URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //getLatestRates();
+        //buildURL();
+    }
+
+    private String buildURL() {
+        Uri baseUri = Uri.parse(BASE_URL);
+        Uri.Builder uribuilder = baseUri.buildUpon();
+        uribuilder.appendQueryParameter("app_id", APP_KEY);
+        Log.i(LOG_TAG, uribuilder.build().toString());
+        return uribuilder.build().toString();
+    }
+
+    private void getLatestRates() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo.isConnected()) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/softplus/rateexchanger/utilities/Constants.java
@@ -0,0 +1,8 @@
+package com.softplus.rateexchanger.utilities;
+
+import com.softplus.rateexchanger.BuildConfig;
+
+public class Constants {
+    public static final String BASE_URL = "https://openexchangerates.org/api/latest.json";
+    public static final String APP_KEY = BuildConfig.API_ACCESS_KEY;
+}
